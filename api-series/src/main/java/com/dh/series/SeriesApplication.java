0aabbc7542f2ecf72777d7e91d7c5b3e99756a77
AguRivarola <agurivarola@gmail.com>
2022-12-12 19:02:51 -0300

    cambio nombre DB

--- api-series/src/main/java/com/dh/series/SeriesApplication.java
+++ api-series/src/main/java/com/dh/series/SeriesApplication.java
@@ -1,9 +1,16 @@
 package com.dh.series;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories
+@EnableRabbit
+@AutoConfiguration
 public class SeriesApplication {
 
 	public static void main(String[] args) {
